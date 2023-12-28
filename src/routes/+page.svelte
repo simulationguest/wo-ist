003329e3734e6bd58fd9484393441e6c2b181682
simulationguest <projekte@leoj.de>
2023-12-28 22:09:26 +0100

    relax line height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <form class="max-w-7xl flex flex-col items-center gap-8" method="GET" action="/find">
 	<h1 class="text-center text-4xl text-slate-400 dark:text-slate-500 font-bold">Wo ist?</h1>
-	<p class="text-3xl text-center">
+	<p class="text-3xl text-center leading-relaxed">
 		Wo ist denn <select name="amenity" class="dark:bg-gray-700 px-1 py-0.5 rounded">
 			<option value="drinking_water"> Trinkwasser </option>
 			<option value="toilet"> eine Toilette </option>
