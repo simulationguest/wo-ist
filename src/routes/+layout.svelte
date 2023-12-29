821cd30bf2874ce41bcfc5fa140c5018f66eb26a
simulationguest <projekte@leoj.de>
2023-12-29 22:24:46 +0100

    add link to source code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,17 @@
 	<title>{tr('app_name')}</title>
 </svelte:head>
 
-<div
-	class="w-full min-h-screen bg-slate-100 dark:bg-slate-900 dark:text-white p-5 max-lg:pt-16 lg:grid place-items-center"
->
-	<slot />
+<div class="bg-slate-100 dark:bg-slate-900 dark:text-white min-h-screen">
+	<div class="w-full min-h-x p-5 max-lg:pt-16 lg:grid place-items-center">
+		<slot />
+	</div>
+	<footer class="text-center dark:text-slate-600">
+		<a href="https://github.com/simulationguest/wo-ist">Source code</a>
+	</footer>
 </div>
+
+<style>
+	.min-h-x {
+		min-height: calc(100vh - 40px);
+	}
+</style>
